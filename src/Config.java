5e85fbdff1c7f81cd57ead34114eb26cd43ecfc4
Jonas Halstrup <jonashalstrup@c-126-244.eduroam.liu.se>
2016-09-05 14:22:40 +0200

    pretty syso

--- src/Config.java
+++ src/Config.java
@@ -11,19 +11,19 @@ public class Config<T> extends AdjustableConfig {
 	}
 
 	public <U> Config<T> by(Function<DBSeparator, Object> f) {
-		System.out.println("Config - Setting classifyer..");
+		System.out.println("Config - Setting <by> classifyer... " + f);
 		this.bySeperator.add(f);
 		return this;
 	}
 
 	public <W> Config<T> and(Function<DBAction, Object> f) {
-		System.out.println("Config - Adding additional action...");
+		System.out.println("Config - Adding additional action... " + f);
 		this.andActions.add(f);
 		return this;
 	}
 
 	public void summary() {
-		System.out.println("This config stores: " + procedure.toString() + " - byProperty: " + bySeperator.toString()
-				+ " - additional actions: " + andActions);
+		System.out.println("procedure: " + procedure.toString() + " - by: " + bySeperator.toString()
+				+ " - and actions: " + andActions);
 	}
 }
--- src/Query.java
+++ src/Query.java
@@ -2,8 +2,8 @@ import java.util.Collection;
 
 public abstract class Query {
 	public static <T> Collection<T> invoke(StoredProcedure storedProcedure) {
-		System.out.println("Invoking the saved Query...");
-		System.out.println("With the following config:");
+		System.out.println("Invoking the Query: " + storedProcedure.toString());
+		System.out.println("With the following Config:");
 		SharedQueryConfingState.getInstance().getCurrentState().summary();
 		;
 
